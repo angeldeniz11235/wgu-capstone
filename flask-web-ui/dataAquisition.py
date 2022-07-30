cb83ff2e8f7635344d197bcdaa3855859ee343d3
Angel D <angeldeniz11235@gmail.com>
2022-07-30 12:13:41 -0700

    started work on py functions for flask

---
+++ flask-web-ui/dataAquisition.py
@@ -0,0 +1,160 @@
+from alpaca_trade_api.rest import REST, TimeFrame
+from alpaca_trade_api.stream import Stream
+import datetime as dt
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation,Dropout
+from tensorflow.keras import layers
+from tensorflow.keras import activations
+from sklearn.metrics import confusion_matrix, classification_report
+
+    # Import the API keys
+def get_data(symbol_, start_date, end_date):
+    
+    api_id = "PKLXRG5MYKH326I8J1DV"
+    api_secret = "TxPfLNWc7HlulXh17NLCX0ser8wrlqXmbjYKriiB"
+
+    # Create the REST API object for paper trading
+    rest_api = REST(api_id, api_secret,'https://paper-api.alpaca.markets')
+    
+    # Get bar data for apple stock from the last 2 years
+    bar_data = rest_api.get_bars(symbol_, TimeFrame.Minute, start=start_date.strftime("%Y-%m-%d"), end=end_date.strftime("%Y-%m-%d"))
+
+    #if bar_data is not empty, then save the data
+    if len(bar_data) > 0:
+        # Save the apple_bars dataframe to a pickle file
+        bar_data.df.to_pickle(f"data/{symbol_}_bar_data.pkl")
+    # else, return an error message
+    else:
+        return "No data found for " + symbol_
+
+
+    #convert the dataframe index to a column
+    bar_data['DateTime'] = bar_data.index.to_pydatetime()
+    # remove the index from the dataframe
+    bar_data = bar_data.reset_index(drop=True)
+    
+    
+    #drop all columns except for the DateTime and the vwap columns
+    bar_data = bar_data.drop(columns=['open', 'high', 'low', 'close', 'volume','trade_count'])
+
+    #create an array of all the unique dates in the dataframe
+    dates = bar_data['DateTime'].dt.date.unique()
+    # make dates to list
+    dates = dates.tolist()
+    dates = list(map(lambda x: x.strftime('%Y-%m-%d'), dates))
+    
+    # in a new dataframe, for each date in the array, create a columns for every vwap value for that date
+    bar_data = pd.DataFrame(index=dates, columns=range(0, 1000))
+
+    # get all vwap values for each date
+    for date in dates:
+        # get the vwap values for that date
+        vwap = bar_data[bar_data['DateTime'].dt.strftime('%Y-%m-%d') == date]['vwap'].tolist()
+        # fill vwap list with zeros to match the length of the columns
+        vwap = vwap + [0] * (1000 - len(vwap))
+        # create a column for that date
+        bar_data.loc[date] = vwap
+
+    #save the dataframe to a pickle file
+    bar_data.to_pickle(f'data/{symbol_}_df.pkl')
+    
+    # set 'gain' column to 1 if column '0' is less than the last non-zero column in the row
+    bar_data['gain'] = bar_data.apply(lambda x: True if x[0] < x[x[x>0].last_valid_index()] else False, axis=1)
+
+    # pickle the dataframe
+    bar_data.to_pickle(f'data/{symbol_}_df_ML.pkl')
+    
+    #drop index column
+    bar_data.reset_index(drop=True, inplace=True)
+    
+    
+    # function drops any rows with more than 40% 0 values
+    def drop_rows_with_many_zeros(df):
+        counts =(df==0).astype(int).sum(axis=1)
+        df = df[counts < len(df.columns)*0.4]
+        return df
+
+    # function that replaces any nan values with the last non-nan value
+    def replace_nan_with_last_non_nan(df):
+        for row_indx in range(len(df)):
+            last_non_nan_value = df.iloc[row_indx,0]
+            for col_indx, val in enumerate(df.iloc[row_indx,:]):
+                if not pd.notna(val):
+                    df.iloc[row_indx,col_indx] = last_non_nan_value
+                else:
+                    last_non_nan_value = val
+        return df
+
+    #only keep values that are not equal to 0 except for the 'gain' column
+    nan_value = float('NaN')
+
+    #replace all 0s with NaN
+    bar_data.replace(0, nan_value, inplace=True)
+
+    #drop rows with more than 40% 0 values
+    bar_data = drop_rows_with_many_zeros(bar_data)
+
+    #replace nan values with the last non-nan value
+    bar_data = replace_nan_with_last_non_nan(bar_data)
+
+
+    # for the 'gain' column, replace all True with 1 and all False with 0
+    bar_data.replace(True, 1, inplace=True)
+    bar_data.replace(False, 0, inplace=True)
+
+    # remove all columns between 360 and 999
+    bar_data.drop(bar_data.columns[360:1000], axis=1, inplace=True)
+
+    # save the dataframe to a pickle file
+    bar_data.to_pickle(f'data/{symbol_}_df_ML.pkl')
+
+
+def train_model(symbol_):
+    
+    #load apple_df_ML.pkl
+    bar_data = pickle.load(open(f'data/{symbol_}_df_ML.pkl', 'rb'))
+    X = bar_data.drop('gain', axis=1).values
+    y = bar_data['gain'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1204)
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    model = Sequential()
+
+    model.add(Dense(units=750, activation=activations.gelu))
+    model.add(Dropout(0.02))
+
+    model.add(Dense(units=1, activation=activations.sigmoid))
+
+    #binary_crossentropy
+    model.compile(loss='binary_crossentropy', optimizer='adam')
+
+    #train the model
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+    model.fit(x=X_train, y=y_train, epochs=1000, batch_size=256, validation_data=(X_test, y_test), verbose=1, callbacks=[callback])
+
+    # if model.predict(X_test) > 0.44 then 1 else 0:
+    predictions = model.predict(X_test)
+    offset = .00
+    predictions[predictions > (predictions.mean() + offset)] = 1
+    predictions[predictions <= (predictions.mean() + offset)] = 0
+    
+    #save the model
+    model.save(f'models/{symbol_}_model_ML.h5')
+    
+def test():
+    end = dt.datetime.now().date() - dt.timedelta(days=1)
+    start = end - dt.timedelta(days=end.day)
+    get_data("dfsdews", start, end)    
+    
+    
+if __name__ == "__main__":
+    test()
